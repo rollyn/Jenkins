075fbf786939830ee18df7e8cb37564e76998711
Ethanium <rollynmoises@gmail.com>
2017-09-28 21:26:40 +0800

    Jenkins test

---
+++ src/test/java/com/ethanium/jenkinsproject/JenkinsProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ethanium.jenkinsproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
